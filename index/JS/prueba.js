f282c4203b15a162df8a83e45b36e92be16fd139
AdrianDanlos <danlosadrian@gmail.com>
2019-11-14 21:42:31 +0100

    carpeta doc añadida

---
+++ index/JS/prueba.js
@@ -0,0 +1,83 @@
+window.onload = function() {
+
+    function logged(){
+        let status = document.getElementById("logged").value;
+        if(status=="true"){
+            document.getElementById("signIn").style.display="none";
+            document.getElementById("logIn").style.display="none";
+
+            document.getElementById("profile").style.display="inline-block";
+            document.getElementById("logOut").style.display="inline-block";
+        }
+    <<<<<<< desarrollo-julen
+    });*/
+let contenedorFormulario = $('#contenedor-formulario');
+contenedorFormulario.load("../PHP/registerForm.html");
+let div = document.createElement("div");
+div.setAttribute("id", "bodyOnForms");
+document.body.appendChild(div);
+
+
+}
+
+function logIn(){
+    let contenedorFormulario = $('#contenedor-formulario');
+    contenedorFormulario.load("../PHP/loginForm.html");
+
+    let div = document.createElement("div");
+    div.setAttribute("id", "bodyOnForms");
+    document.body.appendChild(div);
+}
+
+function showAd(){
+    let contenedorFormulario = $('#contenedor-formulario');
+    $("#search_categoria option").bind("click", function() {
+        let hol=$(this).attr('value');
+        let formContent ="action=getlink&link="+hol;
+        contenedorFormulario.load('myserv.php',formContent)
+    });
+=======
+else{
+        document.getElementById("signIn").style.display="inline-block";
+        document.getElementById("logIn").style.display="inline-block";
+    >>>>>>> desarrollo
+
+        document.getElementById("profile").style.display="none";
+        document.getElementById("logOut").style.display="none";
+    }
+}
+
+function signIn(){
+    /*$.ajax({
+        url: "../PHP/registerForm.html",
+        type:'GET',
+        dataType: 'html',
+        success: function(data){
+            console.log(typeof data)
+            div.appendChild(data);
+        }
+    });*/
+    let contenedorFormulario = $('#contenedor-formulario');
+    contenedorFormulario.load("../PHP/registerForm.html");
+}
+
+function logIn() {
+    let contenedorFormulario = $('#contenedor-formulario');
+    contenedorFormulario.load("../PHP/loginForm.html");
+}
+
+function showAd(){
+    let contenedorFormulario = $('#contenedor-formulario');
+    $("#search_categoria option").bind("click", function() {
+        let hol=$(this).attr('value');
+        let formContent ="action=getlink&link="+hol;
+        contenedorFormulario.load('myserv.php',formContent)
+    });
+}
+
+
+$('#button-edit-perfil').on('click', function () {
+    $('#edit-user-form').css('display', 'block');
+});
+
+};
