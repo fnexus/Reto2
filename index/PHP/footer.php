2fa163481f8342e9b88a7d8896fff28ec8b6e77a
AdrianDanlos <danlosadrian@gmail.com>
2019-11-15 23:40:51 +0100

    social media footer

--- index/PHP/footer.php
+++ index/PHP/footer.php
@@ -1,5 +1,11 @@
 <footer>
-
+    <div class="share-button">
+        <span><i class="fas fa-share-alt"></i> Share!</span>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+    </div>
 </footer>
 </body>
 </html>
